eb86c806fc6e40ecec200f61616adb73a29c3200
Quang Vo <quanghuyvo90@gmail.com>
2023-09-29 18:58:21 -0700

    Redux for All

--- src/features/partners/partnersSlice.js
+++ src/features/partners/partnersSlice.js
@@ -1,4 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit';
 import { PARTNERS } from '../../app/shared/PARTNERS'
 
-export const selectAllPartners = () => PARTNERS;
-export const selectFeaturedPartner = () => PARTNERS.find((partner) => partner.featured);
+const initialState = {
+    partnersArray: PARTNERS
+};
+
+const partnersSlice = createSlice({
+    name: 'partners',
+    initialState
+});
+
+export const partnersReducer = partnersSlice.reducer;
+
+export const selectAllPartners = (state) => state.partners.partnersArray;
+export const selectFeaturedPartner = (state) => {
+    return state.partners.partnersArray.find((partner) => partner.featured);
+};
